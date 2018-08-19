f2de2b5c221da8cf3e84643b5faaa798d1cdc74b
Kim Ngoc Huynh <knhuynhtctv@gmail.com>
2018-08-19 13:24:59 +0700

    Add admin page and administration controller

---
+++ app/controllers/admin/administration_controller.rb
@@ -0,0 +1,4 @@
+class Admin::AdministrationController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'administration/index'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get 'home', to: 'application#index'
