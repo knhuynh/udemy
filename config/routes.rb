588c95a15fd44d8a7dd6b09abd12b21bf4f44f5e
Kim Ngoc Huynh <knhuynhtctv@gmail.com>
2018-08-21 23:06:30 +0700

    Update routes for admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'administration/index'
+    get '/', to: 'administration#index'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
